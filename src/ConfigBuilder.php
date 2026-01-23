2f0f55a8997d3dd50f7f62d15eed382ed78e67da
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-01-23 15:56:01 +0300

    Minor refactor

--- src/ConfigBuilder.php
+++ src/ConfigBuilder.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 namespace Yiisoft\CodeStyle;
 
 use PhpCsFixer\Config;
+use PhpCsFixer\ConfigInterface;
 use Yiisoft\CodeStyle\Sets\YiisoftCoreRiskySet;
 use Yiisoft\CodeStyle\Sets\YiisoftCoreSet;
 
 final class ConfigBuilder
 {
-    public static function build(): Config
+    public static function build(): ConfigInterface
     {
-        $config = (new Config());
-        $config->registerCustomRuleSets([
-            new YiisoftCoreSet(),
-            new YiisoftCoreRiskySet(),
-        ]);
-
-        return $config;
+        return (new Config())
+            ->registerCustomRuleSets([
+                new YiisoftCoreSet(),
+                new YiisoftCoreRiskySet(),
+            ]);
     }
 }
