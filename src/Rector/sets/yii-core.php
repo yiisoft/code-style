d7485ab5800f00bf14a0e884c2a4d2297a74aa1b
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-09-18 17:58:30 +0300

    Remove `NullToStrictStringFuncCallArgRector` from rector rules list (#15)

--- src/Rector/sets/yii-core.php
+++ src/Rector/sets/yii-core.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Yiisoft\CodeStyle\Rector\Rules\RemoveOverrideAttributeRector;
 
@@ -18,6 +17,5 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->skip([
         ClosureToArrowFunctionRector::class,
         ReadOnlyPropertyRector::class,
-        NullToStrictStringFuncCallArgRector::class,
     ]);
 };
